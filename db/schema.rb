2a6d597dec53a73281dd8a730f0c2bda4ce6054d
Masao SAKAI <sakai@cite.tohoku.ac.jp>
2020-04-21 17:08:21 +0900

    ユーザー一覧画面追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_04_21_074752) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "sex"
+    t.date "birthday"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
